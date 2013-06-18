264758e1abdf8911dc5e1a962eb7af05ed8d3353
Emilio Mena <emilio.mena@bqreaders.com>
2013-06-18 14:36:26 +0200

    Fixed Rquest Error JSON serialization

--- client/SimpleChat/src/org/ejmc/android/simplechat/model/RequestError.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/model/RequestError.java
@@ -10,7 +10,7 @@ import com.google.gson.annotations.SerializedName;
  */
 public class RequestError {
 
-	@SerializedName("message")
+	@SerializedName("error")
 	private String message;
 
 	public String getMessage() {
