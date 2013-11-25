063e05271fab20d57fcc60683b76c7389c610023
monterobq <joseluis.montero@bqreaders.com>
2013-11-25 16:29:00 +0100

    Inserción de archivos

--- client/SimpleChat/src/org/ejmc/android/simplechat/ChatActivity.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/ChatActivity.java
@@ -1,27 +1,118 @@
 package org.ejmc.android.simplechat;
+import  org.ejmc.android.simplechat.model.*;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
+//import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+
+import java.util.ArrayList;
 
 /**
  * Chat activity.
  * 
  * @author startic
  */
-public class ChatActivity extends Activity {
+public class ChatActivity extends ListActivity {
+    /** Called when the activity is first created. */
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_chat);
+    private EditText editor;
+    private ImageButton boton;
+    private TextView nombre;
+    private String nick;
+    private ArrayList<Message> listado;
+    private Adaptador adaptador;
+    private ListView lv;
 
-		// Show the Up button in the action bar.
-		setupActionBar();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
 
-	}
+        nick = getIntent().getExtras().getString("nick");
+        nombre=(TextView) findViewById(R.id.nick);
+        nombre.setText("¡¡¡Bienvenido "+nick+"!!!");
+        editor = (EditText) findViewById(R.id.editTextMensaje);
+        boton = (ImageButton) findViewById(R.id.sendButton);
+
+        listado =
+                new ArrayList<Message>();
+
+        listado.add(
+                new Message("Nombre 1", "Hola"));
+
+        listado.add(
+                new Message("Nombre 2", "Que pasa?"));
+
+        listado.add(
+                new Message("Nombre 3", "Aqui estamos"));
+
+        listado.add(
+                new Message("Nombre 4", "Ok"));
+
+        adaptador = new Adaptador(ChatActivity.this, listado);
+        //setListAdapter((ListAdapter) adaptador);
+        recargar();
+        lv=(ListView)this.getListView();
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = editor.getText().toString();
+                if( hayCaracter(msg)){//lo envio si no no
+                    Message m= listado.get(listado.size()-1);
+                    if(m.getNombre().equals(nick)){
+                        String lastmessage=  listado.get(listado.size()-1).getMensaje();
+                        //listado.get(listado.size()).setMensaje(listado.get(listado.size()).getMensaje()+"/n"+msg);
+                        listado.remove(listado.size()-1);
+                        listado.add(new Message(nick, lastmessage+"\n"+msg));
+
+                    } else{
+                        listado.add(new Message(nick, msg));
+                    }
+
+
+                    recargar();
+                    lv.setSelection(lv.getAdapter().getCount()-1);
+                }
+                editor.setText("");
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Read values from the "savedInstanceState"-object and put them in your textview
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the values you need from your textview into "outState"-object
+        super.onSaveInstanceState(outState);
+    }
+
+    public boolean hayCaracter( String cadena ){
+        for(int i=0; i<cadena.length()-1; i++){
+            if(cadena.charAt(i)!=' ' && cadena.charAt(i)!='\n' && cadena.charAt(i)!='\t' )//tiene algun caracter distinto de ' '
+                return true;
+        }
+        return false;
+    }
+
+    public void recargar(){
+
+        //adaptador = new Adaptador(
+        //        Chat.this, listado);
+        setListAdapter((ListAdapter) adaptador);
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,7 +124,7 @@ public class ChatActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			NavUtils.navigateUpFromSameTask(this);
+			//NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
--- client/SimpleChat/src/org/ejmc/android/simplechat/model/Message.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/model/Message.java
@@ -9,5 +9,19 @@ package org.ejmc.android.simplechat.model;
  */
 public class Message {
 
-	
+    private String nombre;
+    private String mensaje;
+
+    public Message(String nombre, String mensaje) {
+        this.nombre = nombre;
+        this.mensaje = mensaje;
+    }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getNombre() { return nombre; }
+
+    public void setMensaje(String mensaje) { this.mensaje = mensaje; }
+
+    public String getMensaje() { return mensaje; }
 }
